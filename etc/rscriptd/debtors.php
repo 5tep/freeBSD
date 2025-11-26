8d5d05c3a2ca5e6251e91888411565b61b9b55fb
5tep <shliepok@gmail.com>
2025-11-26 16:25:15 +0300

    подключени к mysql серверу

--- etc/rscriptd/debtors.php
+++ etc/rscriptd/debtors.php
@@ -1,13 +1,24 @@
 #!/usr/local/bin/php
 <?php
- 
-$config=parse_ini_file(dirname(__FILE__)."/config");
-$link = mysql_connect($config['host'], $config['username'], $config['password']);
-mysql_select_db($config['database']);
-$data_q='SELECT `ip` FROM `users` WHERE `Cash`< -`Credit`';
-$data=mysql_query($data_q);
-           while ($row = mysql_fetch_array($data, MYSQL_ASSOC)) {
-                shell_exec("/sbin/ipfw -q table 47 add ".$row['ip']);
-           }
- 
+
+$config = parse_ini_file(dirname(__FILE__) . "/config");
+
+// Подключение через mysqli
+$mysqli = new mysqli($config['host'], $config['username'], $config['password'], $config['database']);
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+$data_q = 'SELECT `ip` FROM `users` WHERE `Cash` < -`Credit`';
+$result = $mysqli->query($data_q);
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        shell_exec("/sbin/pfctl -q -t block_users -T add " . escapeshellarg($row['ip']));
+    }
+    $result->free();
+}
+
+$mysqli->close();
 ?>
--- etc/rscriptd/mysql.php
+++ etc/rscriptd/mysql.php
@@ -1,35 +1,43 @@
 <?php
-$config=parse_ini_file(dirname(__FILE__)."/config");
-if (extension_loaded('mysqli')) {
-    $loginDB = new mysqli($config['host'], $config['username'], $config['password'], $config['database']);
-    function DB_query($query) {
-        global $loginDB;
-        $result = $loginDB->query($query);
-        return($result);
-    }
-    function DB_real_escape_string($parametr) {
-        global $loginDB;
-        $result = $loginDB->real_escape_string($parametr);
-        return($result);
-    }
-    function DB_fetch_array($data) {
-        $result = mysqli_fetch_assoc($data);
-        return($result);
-    }
-} else {
-    $link = mysql_connect($config['host'], $config['username'], $config['password']);
-    mysql_select_db($config['database']);
-    function DB_query($query) {
-        $result = mysql_query($query);
-        return($result);
-    }
-    function DB_real_escape_string($parametr) {
-        $result = mysql_real_escape_string($parametr);
-        return($result);
-    }
-    function DB_fetch_array($data) {
-        $result = mysql_fetch_array($data, MYSQL_ASSOC);
-        return($result);
-    }
+
+$config = parse_ini_file(__DIR__ . '/config');
+
+// Создаём подключение один раз
+$loginDB = new mysqli(
+    $config['host'],
+    $config['username'],
+    $config['password'],
+    $config['database']
+);
+
+// Проверяем ошибки подключения
+if ($loginDB->connect_error) {
+    error_log("MySQL connection failed: " . $loginDB->connect_error);
+    die("Database connection failed");
 }
-?>
+
+// Устанавливаем кодировку (рекомендуется)
+$loginDB->set_charset('utf8');
+
+// Универсальные функции (теперь без global и дублирования)
+
+function DB_query($query) {
+    global $loginDB;
+    $result = $loginDB->query($query);
+    if (!$result) {
+        error_log("Query failed: " . $loginDB->error . " | Query: " . $query);
+    }
+    return $result;
+}
+
+function DB_real_escape_string($string) {
+    global $loginDB;
+    return $loginDB->real_escape_string($string);
+}
+
+function DB_fetch_array($result) {
+    if ($result instanceof mysqli_result) {
+        return $result->fetch_assoc();
+    }
+    return null;
+}
